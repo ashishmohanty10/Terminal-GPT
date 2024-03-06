0dc4cec3baae88dd182f862af3a369f71c6c5c5b
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-03-06 21:26:14 +0530

    almost done

---
+++ index.js
@@ -0,0 +1,41 @@
+import openai from "./config/open-ai.js";
+import readlineSync from "readline-sync";
+import colors from "colors";
+
+async function main() {
+  console.log(colors.bold.green("Welcome to terminal-chatbot"));
+  console.log(colors.bold.green("You can start chatting with the bot"));
+
+  const chatHistory = [];
+
+  while (true) {
+    const userInput = readlineSync.question(colors.yellow("You: "));
+
+    try {
+      const messages = chatHistory.map(([role, content]) => ({
+        role,
+        content,
+      }));
+
+      messages.push({ role: "user", content: userInput });
+
+      const chatCompletion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+      const completionText = chatCompletion.choices[0].message.content;
+      if (userInput.toLocaleLowerCase() === "exit") {
+        console.log(colors.green("Bot: " + completionText));
+        return;
+      }
+      console.log(colors.green("Bot: " + completionText));
+
+      chatHistory.push(["user", userInput]);
+      chatHistory.push(["assitant", completionText]);
+    } catch (err) {
+      console.log(colors.red(err));
+    }
+  }
+}
+
+main();
